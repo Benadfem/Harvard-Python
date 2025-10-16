734fb7f38a8dc92f723d4caae4b8eaeedf482375
unknown <adedoyin.adedara@gmail.com>
2025-10-17 01:34:39 +0200

    Added the split method on the string name.

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@
 #Ask the user for thier name and removing the white spaces and Capitalizing the name
 name = input("What's your name? ").strip().title()
 
+
+#Greeting the usser only with the first name 
+first, last =name.split()
 #Say Hello to User!
 # Added the F string formatting 
-print(f"Hello, {name}")
+print(f"Hello, {first}")
