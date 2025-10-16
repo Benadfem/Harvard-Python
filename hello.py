4fad0495e4fe33e8d6cd6d89462157e68e49d0be
unknown <adedoyin.adedara@gmail.com>
2025-10-16 18:15:39 +0200

    The program was able to say hello to a user's name

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello World")
+#Passing argument to function by 
+# telling the user to input thier name
+
+#Ask the user for thier name
+name = input("What's your name? ")
+
+#Say Hello to User!
+print("Hello,", name)
