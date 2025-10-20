103cb1af5c3a83564659988ddf6e9a6436ce3aea
unknown <adedoyin.adedara@gmail.com>
2025-10-20 21:59:08 +0200

    using the main function is a very good practice in python programming it allows you to define any function at any point in the code. all you need do is call the function defined in the main function

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+
+def main():
+    # call the function to action and adding the name of the user 
+    name = input("What's your name> ")
+    hello(name)
+
+
 # writing hello program to be done using function 
 def hello(to):
     print(f"Hello, {to}")
 
-name = input("What's your name> ")
-hello(name)
+main()
