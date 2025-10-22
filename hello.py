966f020f6a7a7a56fe4e9a20f1fb8de0d548d01c
unknown <adedoyin.adedara@gmail.com>
2025-10-22 22:14:36 +0200

    return was added to the function so that we can do more than what we did in the function

--- hello.py
+++ hello.py
@@ -2,11 +2,13 @@
 def main():
     # call the function to action and adding the name of the user 
     name = input("What's your name> ")
-    hello(name)
+    greeting = hello(name) + " How are you doing today "
+    print(greeting)
 
 
 # writing hello program to be done using function 
 def hello(to):
-    print(f"Hello, {to}")
+    # print(f"Hello, {to}")
+    return f"Hello. {to}"
 
 main()
