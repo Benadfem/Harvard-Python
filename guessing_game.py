9eb43c80d4689eb44a4a192577d2714037843ff8
HomePC <adedoyin.adedara@gmail.com>
2026-01-06 20:25:29 +0100

    The guess game is completely done without a reference

--- guessing_game.py
+++ guessing_game.py
@@ -5,4 +5,37 @@ import random
 lowest_number = 1
 highest_number = 20
 
-#let's give
+#let's give the count of guesses before the match
+guesses = 0
+is_running = True
+
+#let's prompt for the guess instruction
+print("--------THIS IS A GUESSING GAME --------")
+
+print(f"Select a number from {lowest_number} to {highest_number}")
+guess_number = random.randint(lowest_number, highest_number)
+while is_running:
+    guess = input("Enter your guess: ")
+    guesses += 1
+    if guess.isdigit():
+        guess = int(guess)
+
+
+        if guess < lowest_number or guess > highest_number:
+            print("Guess is Out of range")
+        elif guess > guess_number:
+            print("Your guess is too high")
+            print("Try again!")
+        elif guess < guess_number:
+            print("Your guess is too low")
+            print("Try again!")
+        else:
+            print("--------- CORRECT  GUESS---------")
+            print(f"The correct guess is {guess}")
+            print(f"the number of guess is: {guesses}")
+            break
+
+    else:
+        print("The guess must be a number ")
+
+
